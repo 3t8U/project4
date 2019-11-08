2b69093f4837d9d806ce3d008f55960709ad456e
Nate Powers <skynet@nathans-MacBook-Pro.local>
2019-11-08 08:50:56 -0800

    add pizza constructor and begin price prototype

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// bussiness logic
+function Pizza(size,toppings){
+  this.size = size;
+  this.toppings = topping;
+}
+
+Pizza.protoype.getPrice = function(){
+this.toppings.foreach(function(topping) {
+  this.price += 1;
+}
+  }
+}
