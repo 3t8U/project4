d2c5cb2d72ee5a1b4d418a738b5978bfc45b4860
Nate Powers <skynet@nathans-MacBook-Pro.local>
2019-11-08 11:09:07 -0800

    add more javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
-// bussiness logic
+// Bussiness Logic
 function Pizza(size,toppings){
   this.size = size;
   this.toppings = topping;
+  this.price = 0;
 }
 
 Pizza.protoype.getPrice = function(){
-this.toppings.foreach(function(topping) {
-  this.price += 1;
-}
+  this.toppings.foreach(function(topping) {
+    this.price += 1;
   }
+  if (this.size === "s"){
+    this.price += 5;
+  } else if (this.size === "m") {
+    this.price += 10;
+  } else {
+    this.price += 15,
+  }
+  return thisprice;
+
+
+
+
 }
+}
+
+
+
+
+// User Logic
+$(document).ready(function(){
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    var toppingsInput = $("input:checkbox[name=toppings-select]:checked").each();
+    var toppingArray = [];
+    var sizeInput = $("#size").val()
+
+
+
+  });
+});
